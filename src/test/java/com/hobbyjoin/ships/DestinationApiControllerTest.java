75b51efd10c154d7b950d3a572f9ad9ed0af7ae4
mczarnecki83 <czarnymic@interia.pl>
2022-02-11 16:57:13 +0100

    Add two unit tests to check add new destination

--- src/test/java/com/hobbyjoin/ships/DestinationApiControllerTest.java
+++ src/test/java/com/hobbyjoin/ships/DestinationApiControllerTest.java
@@ -6,11 +6,13 @@ import com.hobbyjoin.ships.model.user.UserDetailsServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -44,5 +46,50 @@ public class DestinationApiControllerTest {
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
         String expected = "{\"id\":null,\"name\":\"PORT_GDYNIA\",\"latitude\":55.4,\"longitude\":66.3,\"description\":null,\"info\":null,\"blackList\":false,\"userId\":null}";
         JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+    }
+
+
+    @Test
+    public void addDestination() throws Exception {
+        String contentJson="{\n" +
+                "    \"name\": \"TEST_ADD222\",\n" +
+                "    \"latitude\": 63.55,\n" +
+                "    \"longitude\": 10.21667,\n" +
+                "    \"description\": null,\n" +
+                "    \"info\": null,\n" +
+                "    \"blackList\": false\n" +
+                "}";
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/destination/add")
+                .content(contentJson)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        System.out.println(result.getResponse().getStatus());
+        final int status = result.getResponse().getStatus();
+        JSONAssert.assertEquals("201",String.valueOf(status),false);
+    }
+
+
+
+    @Test
+    public void addDestinationNameNull() throws Exception {
+        String contentJson="{\n" +
+                "    \"name\": \"\",\n" +
+                "    \"latitude\": 63.55,\n" +
+                "    \"longitude\": 10.21667,\n" +
+                "    \"description\": null,\n" +
+                "    \"info\": null,\n" +
+                "    \"blackList\": false\n" +
+                "}";
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/destination/add")
+                .content(contentJson)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        System.out.println(result.getResponse().getStatus());
+        final int status = result.getResponse().getStatus();
+        JSONAssert.assertEquals("400",String.valueOf(status),false);
     }
 }
